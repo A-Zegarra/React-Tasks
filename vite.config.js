f5167d0b27766a4271edc7a1e623f1733c98281c
Alvaro Zegarra <argz.zegarra@gmail.com>
2025-08-26 22:01:49 -0500

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/React-Tasks/'
 })
