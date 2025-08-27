64a1e6251c2c2f5f586e9a82d59f2e9e1b41650d
Alvaro Zegarra <argz.zegarra@gmail.com>
2025-08-26 20:55:26 -0500

    first commit

---
+++ src/tasks.js
@@ -0,0 +1,23 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'mi 1 tarea',
+        description: 'mi 1 tarea'
+    },
+    {
+        id: 1,
+        title: 'mi 2 tarea',
+        description: 'mi 2 tarea'
+    },
+    {
+        id: 2,
+        title: 'mi 3 tarea',
+        description: 'mi 3 tarea'
+    },
+    {
+        id: 3,
+        title: 'mi 4 tarea',
+        description: 'mi 4 tarea'
+    },
+
+]
